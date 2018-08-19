6c987be98f1ad998eab07a8543ef1071af1866b4
anchorage418 <37402865+anchorage418@users.noreply.github.com>
2018-08-19 18:53:36 +0300

    material icons

--- src/components/ResultsLayoutForm.jsx
+++ src/components/ResultsLayoutForm.jsx
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import Icon from '@mdi/react';
+import { 
+  mdiNumeric1BoxMultipleOutline,
+  mdiNumeric2BoxMultipleOutline,
+  mdiNumeric3BoxMultipleOutline,
+  mdiNumeric4BoxMultipleOutline,
+} from '@mdi/js';
 
 class ResultsLayoutForm extends Component {
 
@@ -17,43 +24,91 @@ class ResultsLayoutForm extends Component {
     return(
       <div className="result-layout result-layout__container">
         <form className="layout-form">
-          <input 
-            type="radio" 
-            id="one"
-            name="column" 
-            value="1" 
-            checked={option === '1'} 
-            onChange={this.handleChange}
-            className="layout-form__input"
-          />
-          <label htmlFor="one">One column</label>
-          <input 
-            type="radio"
-            id="two"
-            name="column"
-            value="2"
-            checked={option === '2'}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="two">Two columns</label>
-          <input
-            type="radio"
-            id="three"
-            name="column"
-            value="3"
-            checked={option === '3'}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="three">Three columns</label>
-          <input
-            type="radio"
-            id="four"
-            name="column" 
-            value="4" 
-            checked={option === '4'} 
-            onChange={this.handleChange}
-          />
-          <label htmlFor="four">Four columns</label>
+          <span className="layout-form__title">
+            Choose columns views:
+          </span>
+          <div className="layout-form__inner-wrapper">
+            <div className="layout-form__input-container">
+              <Icon 
+                path={ mdiNumeric1BoxMultipleOutline }
+                size={2}
+                color="#fff"
+              />
+              <input 
+                type="radio" 
+                id="one"
+                name="column" 
+                value="1" 
+                checked={option === '1'} 
+                onChange={this.handleChange}
+                className="layout-form__input"
+              />
+              <label 
+                htmlFor="one"
+                className="layout-form__label"
+              ></label>
+            </div>
+            <div className="layout-form__input-container">
+              <Icon 
+                path={ mdiNumeric2BoxMultipleOutline }
+                size={2}
+                color="#fff"
+              />
+              <input 
+                type="radio"
+                id="two"
+                name="column"
+                value="2"
+                checked={option === '2'}
+                onChange={this.handleChange}
+                className="layout-form__input"
+              />
+              <label 
+                htmlFor="two"
+                className="layout-form__label"
+              ></label>
+            </div>
+            <div className="layout-form__input-container">
+              <Icon 
+                path={ mdiNumeric3BoxMultipleOutline }
+                size={2}
+                color="#fff"
+              />
+              <input
+                type="radio"
+                id="three"
+                name="column"
+                value="3"
+                checked={option === '3'}
+                onChange={this.handleChange}
+                className="layout-form__input"
+              />
+              <label 
+                htmlFor="three"
+                className="layout-form__label"
+              ></label>
+            </div>
+            <div className="layout-form__input-container">
+              <Icon 
+                path={ mdiNumeric4BoxMultipleOutline }
+                size={2}
+                color="#fff"
+              />
+              <input
+                type="radio"
+                id="four"
+                name="column" 
+                value="4" 
+                checked={option === '4'} 
+                onChange={this.handleChange}
+                className="layout-form__input"
+              />
+              <label 
+                htmlFor="four"
+                className="layout-form__label"
+              ></label>
+            </div>
+          </div>
         </form>
         {React.cloneElement(children, {columnCount: count})}
       </div>
